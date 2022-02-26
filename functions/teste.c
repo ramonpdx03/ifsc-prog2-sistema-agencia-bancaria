73e87b2665be1048d40587d7c71739e2b3ca22e4
Ramon <ramonog03@gmail.com>
2022-02-25 21:54:50 -0300

    Arquivos iniciais

---
+++ functions/teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int soma() {
+
+    int a, b;
+
+    a = 2;
+    b = 37;
+    
+    return (a + b);
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "functions/teste.c"
+
+int main() {
+
+    printf("Hello World %d", soma());
+
+    return 0;
+}
